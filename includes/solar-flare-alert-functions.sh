1ea5cd2faaff98de96addd39c986395ed06a4cc0
Curtis K <curtis@linux.com>
2016-02-29 23:35:13 +1100

    removed redundant subject and notification message variables

--- includes/solar-flare-alert-functions.sh
+++ includes/solar-flare-alert-functions.sh
@@ -13,8 +13,6 @@ update_flare_data() {
 }
 
 send_mail_alert() {
-  local subject='Solar flare alert!'
-  local alert_message="A ${last_flare_event_class}${last_flare_event_scale} class solar flare occurred at ${last_flare_event_time} UTC."
   [ -z "${email_to_notify}" ] && abrt "No mail recipients specified"
 
   for email_address in "${email_to_notify[@]}" ; do
